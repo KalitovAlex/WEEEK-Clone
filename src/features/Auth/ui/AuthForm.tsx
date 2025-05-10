a1a0738474a2e1013f7ab193ac444368a4e8f700
KalitovAlex <Wersuser2@gmail.com>
2025-05-10 19:21:41 +0300

    #dev

--- src/features/Auth/ui/AuthForm.tsx
+++ src/features/Auth/ui/AuthForm.tsx
@@ -1,3 +1,29 @@
+import "./AuthForm.scss";
+import { Form } from "@/shared/ui/Form";
+import { Lock, Mail } from "lucide-react";
+import { authFormSchema } from "../model/types";
+
 export const AuthForm = () => {
-  return <div>AuthForm</div>;
+  return (
+    <div className="auth-form">
+      <Form
+        schema={authFormSchema}
+        fields={[
+          {
+            name: "email",
+            label: "Email",
+            type: "email",
+            icon: <Mail className="auth-form__mail" size={20} />,
+          },
+          {
+            name: "password",
+            label: "Password",
+            type: "password",
+            icon: <Lock className="auth-form__lock" size={20} />,
+          },
+        ]}
+        onSubmit={() => {}}
+      />
+    </div>
+  );
 };
--- src/pages/AuthPage/Auth.page.tsx
+++ src/pages/AuthPage/Auth.page.tsx
@@ -1,29 +1,13 @@
 import { Logo } from "@/shared/ui/Logo";
 import "./Auth.page.scss";
-import { Form } from "@/shared/ui/Form";
-import { z } from "node_modules/zod/lib";
-import { Mail } from "lucide-react";
+import { AuthForm } from "@/features/Auth/ui/AuthForm";
 
 export const AuthPage = () => {
   return (
     <div className="auth-page">
       <div className="auth-page__form">
         <Logo />
-        <Form
-          schema={z.object({
-            email: z.string().email(),
-            password: z.string().min(8),
-          })}
-          fields={[
-            {
-              name: "email",
-              label: "Email",
-              type: "email",
-              icon: <Mail />,
-            },
-          ]}
-          onSubmit={() => {}}
-        />
+        <AuthForm />
       </div>
     </div>
   );

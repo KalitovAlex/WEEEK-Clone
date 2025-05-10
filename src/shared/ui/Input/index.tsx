fdb33a99170273f70af491df4a33abc990b02e1f
KalitovAlex <Wersuser2@gmail.com>
2025-05-10 17:14:42 +0300

    #dev

--- src/shared/ui/Input/index.tsx
+++ src/shared/ui/Input/index.tsx
@@ -1,5 +1,23 @@
 import "./index.scss";
+import { forwardRef } from "react";
 
-export const Input = () => {
-  return <div className="input">Input</div>;
-};
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  type?: "text" | "email" | "password" | "number" | "search" | "tel" | "url";
+  placeholder?: string;
+}
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ type = "text", placeholder, ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        type={type}
+        className="input"
+        placeholder={placeholder}
+        {...props}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";

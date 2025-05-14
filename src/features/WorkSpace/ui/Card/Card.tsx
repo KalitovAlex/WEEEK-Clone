7266588908862c51cb6233bb442a39221535d10e
KalitovAlex <Wersuser2@gmail.com>
2025-05-14 14:34:19 +0300

    #dev

--- src/features/WorkSpace/ui/Card/Card.tsx
+++ src/features/WorkSpace/ui/Card/Card.tsx
@@ -3,11 +3,23 @@ import "./Card.scss";
 import { useTranslation } from "react-i18next";
 import { getItem } from "@/shared/utils/localstorage";
 import { WORKSPACE_NAME } from "@/shared/constants/app";
+import { ChevronLeft, Menu } from "lucide-react";
+import { useState } from "react";
 
 export const WorkSpaceCard = () => {
   const { t } = useTranslation();
+  const [isExpanded, setIsExpanded] = useState(true);
+
+  const handleExpand = () => {
+    const sidebar = document.querySelector(".sidebar");
+    setIsExpanded((prev) => !prev);
+    if (sidebar) {
+      sidebar.classList.toggle("collapsed");
+    }
+  };
+
   return (
-    <div className="work-space-card">
+    <div className={`work-space-card `}>
       <div className="work-space-card__content">
         <Avatar initials="AB" />
         <div className="work-space-card__main">
@@ -17,6 +29,19 @@ export const WorkSpaceCard = () => {
           <h2 className="work-space-card__main__name">
             {getItem(WORKSPACE_NAME)}
           </h2>
+        </div>
+        <div className="work-space-card__actions">
+          {isExpanded ? (
+            <ChevronLeft
+              onClick={handleExpand}
+              className="work-space-card__actions__icon"
+            />
+          ) : (
+            <Menu
+              onClick={handleExpand}
+              className="work-space-card__actions__icon"
+            />
+          )}
         </div>
       </div>
     </div>

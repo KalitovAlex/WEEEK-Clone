7089c9af24240f6530a937f1a6d431014e661abb
KalitovAlex <Wersuser2@gmail.com>
2025-05-15 12:13:15 +0300

    #dev

--- src/widgets/Sidebar/ui/index.tsx
+++ src/widgets/Sidebar/ui/index.tsx
@@ -4,11 +4,20 @@ import "./index.scss";
 import { Search } from "lucide-react";
 import { useSidebar } from "@/features/Sidebar/model";
 import { useState } from "react";
+import classNames from "classnames";
 
 export const Sidebar = () => {
   const { t } = useTranslation();
   const { isExpanded: isSidebarExpanded, toggleSidebar } = useSidebar();
   const [isProjectsExpanded, setIsProjectsExpanded] = useState(true);
+
+  const titleClassName = classNames(
+    "sidebar__content__projects__header__title",
+    {
+      "sidebar__content__projects__header__title--collapsed":
+        !isProjectsExpanded,
+    }
+  );
 
   return (
     <div className={`sidebar ${isSidebarExpanded ? "expanded" : "collapsed"}`}>
@@ -29,24 +38,24 @@ export const Sidebar = () => {
             className="sidebar__content__projects__header"
             onClick={() => setIsProjectsExpanded((prev) => !prev)}
           >
-            <h3 className="sidebar__content__projects__header__title">
-              {t("project.title")}
-            </h3>
-            <div className="sidebar__content__projects__header__actions">
-              {!isProjectsExpanded && (
-                <h3 className="sidebar__content__projects__header__actions__title">
-                  {t("project.hidden", { count: 0 })}
-                </h3>
-              )}
-              <img
-                onClick={(e) => {
-                  e.stopPropagation();
-                }}
-                className="sidebar__content__projects__header__actions__add"
-                src="/add.svg"
-                alt="add"
-              />
-            </div>
+            <h3 className={titleClassName}>{t("project.title")}</h3>
+            {isSidebarExpanded && (
+              <div className="sidebar__content__projects__header__actions">
+                {!isProjectsExpanded && (
+                  <h3 className="sidebar__content__projects__header__actions__title">
+                    {t("project.hidden", { count: 0 })}
+                  </h3>
+                )}
+                <img
+                  onClick={(e) => {
+                    e.stopPropagation();
+                  }}
+                  className="sidebar__content__projects__header__actions__add"
+                  src="/add.svg"
+                  alt="add"
+                />
+              </div>
+            )}
           </div>
         </div>
       </div>

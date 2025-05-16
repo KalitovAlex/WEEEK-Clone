63cc0807209b1b8318c59e45a28fc14418fcaf7b
KalitovAlex <Wersuser2@gmail.com>
2025-05-16 10:10:56 +0300

    #dev

--- src/features/Boards/ui/Card/index.tsx
+++ src/features/Boards/ui/Card/index.tsx
@@ -1,12 +1,20 @@
 import { Avatar } from "@/shared/ui/Avatar";
 import "./index.scss";
 import type { Board } from "@/entities/Boards/model";
-import { LucideEllipsis } from "lucide-react";
+import { LucideEllipsis, LucidePencil } from "lucide-react";
+import { Popover } from "@/shared/ui/Popover";
 
 interface BoardCardProps {
   board: Board;
 }
 export const BoardCard = ({ board }: BoardCardProps) => {
+  const items = [
+    {
+      label: "Edit",
+      icon: <LucidePencil />,
+      value: "edit",
+    },
+  ];
   return (
     <div className="board-card">
       <div className="board-card__content">
@@ -14,7 +22,9 @@ export const BoardCard = ({ board }: BoardCardProps) => {
         <p className="board-card__content__title">{board.title}</p>
       </div>
       <div className="board-card__actions">
-        <LucideEllipsis className="board-card__actions__icon" />
+        <Popover items={items}>
+          <LucideEllipsis className="board-card__actions__icon" />
+        </Popover>
       </div>
     </div>
   );
